1e51660ff4a3fbf3ee1781490e9322c9652410f2
Paul Scarrone <pscarrone@thinkthroughmath.com>
2015-07-14 16:03:29 -0400

    more expressive functional extraction of a group of highly probably letters

--- lib/pronounceable_password.rb
+++ lib/pronounceable_password.rb
@@ -1,6 +1,9 @@
 require_relative './letter_probability'
 
 class PronounceablePassword
+
+  LETTERS = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
+
   def initialize
     prob = LetterProbability.new
     prob.load_dictionary
@@ -8,12 +11,15 @@ class PronounceablePassword
   end
 
   def best_next_letter(letter)
-    letters = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
-    matches = letters.map do |l|
+    matches = LETTERS.map do |l|
       prob = @prob_hash.fetch("#{letter}#{l}", 0)
       {"#{letter}#{l}" => prob}
     end
-    best_match = matches.max_by{|x| x.first.last}
+    best_match = matches.sort_by{ |x| x.first.last }
+                        .reject{ |x| x.first.last < 1 }
+                        .reverse
+                        .slice(0, 2)
+                        .sample
     best_match.first.first.split("").last
   end
 end
