fa82ad852e7be7e6fc90166e98270b40f409da58
Paul Scarrone <pscarrone@thinkthroughmath.com>
2015-07-14 16:03:47 -0400

    start each driver password with a random letter

--- app.rb
+++ app.rb
@@ -1,7 +1,8 @@
 require_relative './lib/pronounceable_password'
 pronounce = PronounceablePassword.new
-pass_length = 5
-password = ['t']
+pass_length = 10
+starting_letter = PronounceablePassword::LETTERS.sample
+password = [starting_letter]
 while password.length < pass_length do 
   password << pronounce.best_next_letter(password[password.length-1])
 end
